e0ecb33126ee17e8de3d2ec1fe29603cc399e040
SadiniWanniarachchi <sadiiw17@gmail.com>
2025-02-10 10:18:11 +0530

    main backend and frontend files are done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
+    <BrowserRouter>
 
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-    </>
-  )
-}
+      </Routes>
 
-export default App
+    </BrowserRouter>
+  );
+};
+
+export default App;
