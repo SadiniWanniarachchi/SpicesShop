767e5c4393ceafab218a9a518fceb8058baabd0c
Wimukthi316 <wimukthi316@gmail.com>
2025-02-23 14:47:02 +0530

    Create a new branch called the shop and completed it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import { Footer } from "./components/Footer";
 import { AboutUs } from "./pages/AboutUs";
 import { ContactUs } from "./pages/ContactUs";
+import  Shop  from "./pages/Shop";
+import  Cart  from "./pages/Cart";
 
 const App = () => {
   return (
@@ -16,6 +18,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
 
